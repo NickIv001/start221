5d96a4cd55e3d452472054c12b6431cf2f821162
NickIv001 <nickivasuta.w@gmail.com>
2021-10-28 21:38:21 +0300

    startofPractical2

---
+++ Pfor005lecture/Practical.task.for.005.lecture.js
@@ -0,0 +1,11 @@
+//Practical task for 005 lecture JavaScript - Language Basics. Part 2.
+//1.Write a JavaScript function to check if given string includes given symbol
+
+let my_string11="aheloo"
+if (my_string11.indexOf('hello') > -1)
+{
+  console.log("hello found inside your_string");
+}
+
+//2
+console.log("hello found inside your_string");
