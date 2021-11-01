a5440394a918bb75b15c9d2177788e6dc96345ef
NickIv001 <nickivasuta.w@gmail.com>
2021-11-01 20:24:33 +0200

    start006

---
+++ PT006/Practical.Tasks.For.006.Lecture.Javascript.basics.p3.js
@@ -0,0 +1,11 @@
+//006 - Practical Tasks Assignment
+
+//1. Write a JavaScript function to get the last element of anarray
+
+
+
+//2. Write a simple JavaScript program to join all elements of the following array into a string
+
+//3. There are two arrays with individual values, write a JavaScript program to compute the sum of each individual index value from the given arrays and save them to third array
+
+//4. Write a JavaScript program to calculate the area and perimeter of a rectangle. Rectangle should be an object with properties width and height and methods getArea() and getPerimeter();
